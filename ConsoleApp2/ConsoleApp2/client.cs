737bf260404fbae9400d09f501d4331f8aa47bd0
harveyworkexperience <45348734+harveyworkexperience@users.noreply.github.com>
2018-12-17 11:00:02 +1030

    Example UDP Client and Server

---
+++ ConsoleApp2/ConsoleApp2/client.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System.Diagnostics;
+
+class Client
+{
+    // A function for listening to server responses
+    static void ReplyThread(UdpClient udpclient, IPEndPoint ep)
+    {
+        while (true)
+        {
+            try
+            {
+                var data = Encoding.ASCII.GetString(udpclient.Receive(ref ep));
+                Console.WriteLine(data);
+                if (string.Equals(data.ToString(),"quit".ToString()))
+                    break;
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex);
+            }
+        }
+        Console.WriteLine("Thread closed");
+    }
+
+    // Starts the thread and provides the threaded function parameters
+    static Thread StartTheThread(UdpClient arg1, IPEndPoint arg2)
+    {
+        var t = new Thread(() => ReplyThread(arg1, arg2));
+        t.Start();
+        return t;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("CLIENT\n=====================");
+
+        var udpclient = new UdpClient();
+        IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000); // endpoint where server is listening
+        udpclient.Connect(ep);
+
+        // Listening to replies on another thread
+        Thread thread1 = StartTheThread(udpclient, ep);
+
+        // Message the server
+        string read = "";
+        while (read != "quit")
+        {
+            read = Console.ReadLine();
+            var datagram = Encoding.ASCII.GetBytes(read);
+            udpclient.Send(datagram, datagram.Length);
+        }
+        Console.WriteLine("Closing client");
+        return;
+    }
+}
---
+++ ConsoleApp2/ExampleUdpServer/server.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.IO;
+
+class Server
+{
+    static void Main()
+    {
+        Console.WriteLine("SERVER\n=====================");
+        UdpClient udpserver = new UdpClient(11000);
+
+        while (true)
+        {
+            var ep = new IPEndPoint(IPAddress.Any, 11000);
+            var data = Encoding.ASCII.GetString( udpserver.Receive(ref ep) ); // Listen on port 11000
+            Console.Write("Received data from " + ep.ToString() + "\n");
+            Console.Write("data: " + data +"\n\n");
+            // Replying back to clients
+            var datagram = Encoding.ASCII.GetBytes("Server has received message: " + data + "\n");
+            if (data == "quit")
+                datagram = Encoding.ASCII.GetBytes("quit");
+            udpserver.Send(datagram, datagram.Length, ep);
+        }
+    }
+}
